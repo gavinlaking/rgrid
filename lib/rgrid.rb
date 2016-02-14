9851bae9456906367da0b07cb6e1bf6088d86b47
Gavin Laking <gavinlaking@gmail.com>
2016-02-14 19:54:30 +0000

    Add some documentation.

--- lib/rgrid.rb
+++ lib/rgrid.rb
@@ -2,5 +2,11 @@
 
 require 'rgrid/version'
 
+# RGrid is an Array-like data structure for Ruby for effectively
+# manipulating multi-dimensional arrays.
+#
+# @api public
+#
 module RGrid
+
 end # RGrid
