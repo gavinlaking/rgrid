a5305aa93c648bdaf8702c77da812b72200d98d9
Gavin Laking <gavinlaking@gmail.com>
2016-02-14 17:42:43 +0000

    Update test_helper to include;
    minitest/pride
    minitest/hell
    
    Also, add 'context' as an alias for 'describe'.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,26 @@
+# frozen_string_literal: true
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'rgrid'
 
 require 'minitest/autorun'
+require 'minitest/pride'
+require 'minitest/hell'
+
+module MiniTest
+
+  class Spec
+
+    # parallelize_me! # uncomment to unleash hell
+    # i_suck_and_my_tests_are_order_dependent! # just incase
+
+    class << self
+
+      alias context describe
+
+    end # Eigenclass
+
+  end # Spec
+
+end # MiniTest
